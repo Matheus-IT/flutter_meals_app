37de0e90a26ef66f750d09ff83bfad7db4ec907e
matheus <matheuscosta.tech@gmail.com>
2024-03-14 22:44:16 -0300

    Using provider when adding or removing from favorites

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -4,14 +4,15 @@ import 'package:meals/models/meal.dart';
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
 
-  void toggleMealFavoriteStatus(Meal meal) {
+  bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
 
     if (mealIsFavorite) {
       state = state.where((m) => m.id != meal.id).toList();
-    } else {
-      state = [...state, meal];
+      return false;
     }
+    state = [...state, meal];
+    return true;
   }
 }
 
